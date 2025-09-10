92e8522a466abcf9261d26d81bdaf7615a225a9d
tass-kwon <tass.kwon@3i.ai>
2025-09-10 16:26:23 +0900

    Update Package.swift
    aosl 제거

--- Package.swift
+++ Package.swift
@@ -15,11 +15,6 @@ let package = Package(
             name: "AgoraRtmKit",
             url: "https://download.agora.io/rtmsdk/release/AgoraRtmKit.xcframework_2.2.5.zip",
             checksum: "856aba19003bcfaf09daac35db6717a068a3a687d2fe99a16809d810afa069bc"
-        ),
-        .binaryTarget(
-            name: "aosl",
-            url: "https://download.agora.io/rtmsdk/release/aosl.xcframework.zip",
-            checksum: "4d1768316f2738e339627cddb3186bb74b5163241e4e47ffd05e83a41a89027d"
         )
     ]
 )
